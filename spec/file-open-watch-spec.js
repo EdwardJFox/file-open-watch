8b802f092c234331522974a0d7ecd37360c05fc3
EdwardJFox <edwardjfox@outlook.com>
2018-01-13 20:48:56 +0000

    Added little css identifier for watching a folder, cleaned up the boilerplate code

--- spec/file-open-watch-spec.js
+++ spec/file-open-watch-spec.js
@@ -15,59 +15,9 @@ describe('FileOpenWatch', () => {
     activationPromise = atom.packages.activatePackage('file-open-watch');
   });
 
-  describe('when the file-open-watch:toggle event is triggered', () => {
-    it('hides and shows the modal panel', () => {
-      // Before the activation event the view is not on the DOM, and no panel
-      // has been created
-      expect(workspaceElement.querySelector('.file-open-watch')).not.toExist();
+  describe('when the file-open-watch:add-folder event is triggered', () => {
+    it('I should write tests for this', () => {
 
-      // This is an activation event, triggering it will cause the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'file-open-watch:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        expect(workspaceElement.querySelector('.file-open-watch')).toExist();
-
-        let fileOpenWatchElement = workspaceElement.querySelector('.file-open-watch');
-        expect(fileOpenWatchElement).toExist();
-
-        let fileOpenWatchPanel = atom.workspace.panelForItem(fileOpenWatchElement);
-        expect(fileOpenWatchPanel.isVisible()).toBe(true);
-        atom.commands.dispatch(workspaceElement, 'file-open-watch:toggle');
-        expect(fileOpenWatchPanel.isVisible()).toBe(false);
-      });
-    });
-
-    it('hides and shows the view', () => {
-      // This test shows you an integration test testing at the view level.
-
-      // Attaching the workspaceElement to the DOM is required to allow the
-      // `toBeVisible()` matchers to work. Anything testing visibility or focus
-      // requires that the workspaceElement is on the DOM. Tests that attach the
-      // workspaceElement to the DOM are generally slower than those off DOM.
-      jasmine.attachToDOM(workspaceElement);
-
-      expect(workspaceElement.querySelector('.file-open-watch')).not.toExist();
-
-      // This is an activation event, triggering it causes the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'file-open-watch:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        // Now we can test for view visibility
-        let fileOpenWatchElement = workspaceElement.querySelector('.file-open-watch');
-        expect(fileOpenWatchElement).toBeVisible();
-        atom.commands.dispatch(workspaceElement, 'file-open-watch:toggle');
-        expect(fileOpenWatchElement).not.toBeVisible();
-      });
     });
   });
 });
--- spec/file-open-watch-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import FileOpenWatchView from '../lib/file-open-watch-view';
-
-describe('FileOpenWatchView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
